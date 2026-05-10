4a8af7e6ced6a4babac2159e815002caabcf1bdf
Humza <142959387+HumzaProfessional@users.noreply.github.com>
2026-05-10 03:43:13 -0400

    Update Zigbee_simulation_push.bash

--- Smart Agriculture: Zigbee_based_crop_data_collector/Zigbee_simulation_push.bash
+++ Smart Agriculture: Zigbee_based_crop_data_collector/Zigbee_simulation_push.bash
@@ -1,4 +1,5 @@
 # sample bash code to run to publish to Zigbee module for testing.
 
-mosquitto_pub -h localhost -t zigbee/node1 \ # change the node1 to represent another sensor
--m '{"temperature":22.5,"humidity":45}' # change the numebers to any value
+# node1 represents a simulated Zigbee sensor node
+mosquitto_pub -h localhost -t zigbee/node1 \
+-m '{"temperature":22.5,"humidity":45}'
