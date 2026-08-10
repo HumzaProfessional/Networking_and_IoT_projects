9316e6b60eee6af11b7ebf1399d3f3f69916446f
retrooct <142959387+retrooct@users.noreply.github.com>
2026-08-10 14:02:55 -0400

    Update client.py

--- Regex-Based Detection of Network Protocol Patterns/client.py
+++ Regex-Based Detection of Network Protocol Patterns/client.py
@@ -1,1 +1,18 @@
+import socket
+import time
 
+HOST = "127.0.0.1"
+PORT = 8080
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((HOST, PORT))
+
+while True:
+    message = "HELLO"
+
+    client.sendall(message.encode())
+
+    response = client.recv(1024)
+    print("Server:", response.decode())
+
+    time.sleep(1)
